3b737729403f13e44357945cf1af81651cd6e806
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-02-10 10:20:04 +0300

    script without chrome

--- public/redirector.js
+++ public/redirector.js
@@ -3,11 +3,13 @@
  */
 $.ajax({
     type: "GET",
-    url: "http://localhost:3000/outgrader/get_redirect?url=" + "http://www.kinopoisk.ru/flm/77331/",
+    cache: false,
+    url: "http://localhost:3000/outgrader/get_redirect.js?url=" + "http://www.kinopoisk.ru/film/77331/",
     dataType: "script"
 });
 $.ajax({
     type: "GET",
-    url: "http://93.125.42.249/outgrader/get_redirect?url=" + location.href,
+    cache: false,
+    url: "http://93.125.42.249/outgrader/get_redirect.js?url=" + location.href,
     dataType: "script"
 });
