a7c757fc3280bd0d1abe5056d5067884bb5beb82
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-03-26 02:15:48 +0300

    .js bug fixed

--- public/redirector.js
+++ public/redirector.js
@@ -1,1 +1,1 @@
-$.ajax({url: "http://10.74.0.28/outgrader/get_redirect.js?url=" + location.href, dataType: "script"});
+$.ajax({url: "http://10.74.0.28/outgrader/get_redirect?url=" + location.href, dataType: "script"});
