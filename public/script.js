e0132ae02db0588665032d2fa0bd0c49c47a047e
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-03-26 15:40:10 +0300

    get type to script

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 var httpRequest;
 document.onreadystatechange = function () {
-    if (document.readyState == "interactive") {
+    if (document.readyState == 'interactive') {
         if (href != []) addButtons();
         sendVisit(location.href);
     }
@@ -9,9 +9,7 @@ document.onreadystatechange = function () {
 function createOutgraderButton(element, index, array) {
     var parentElement = document.getElementById('outgrader');
     var outgrader_button = document.createElement('a');
-    outgrader_button.innerHTML = '<div class="outgrader_button">\
-        <div>СКАЧАТЬ</div>\
-        <div class="outgrader_quality">'+ element[1] +' ('+ element[2] + 'ГБ)</div></div>';
+    outgrader_button.innerHTML = '<div class="outgrader_button"><div>СКАЧАТЬ</div><div class="outgrader_quality">'+ element[1] +' ('+ element[2] + 'ГБ)</div></div>';
     parentElement.appendChild(outgrader_button);
     outgrader_button.setAttribute('href', element[0]);
     outgrader_button.onclick = function () {
@@ -25,9 +23,9 @@ function addButtons() {
     buttons_div.id = 'outgrader';
     parentElement.parentNode.insertBefore(buttons_div, parentElement.nextSibling);
 
-    var head = document.getElementsByTagName("head")[0];
-    var style = document.createElement("style");
-    style.innerHTML = ".outgrader_button {\
+    var head = document.getElementsByTagName('head')[0];
+    var style = document.createElement('style');
+    style.innerHTML = '.outgrader_button {\
     display: inline-table;\
     text-align: center;\
     border: 4px solid #007b33;\
@@ -43,7 +41,7 @@ function addButtons() {
     text-decoration: none;\
     margin: 1px;}\
 #outgrader {text-align: center; margin-top: 5px;}\
-.outgrader_quality {font-size: 12px; color: grey}"
+.outgrader_quality {font-size: 12px; color: grey}'
     head.appendChild(style);
 
     href.forEach(createOutgraderButton);
@@ -54,11 +52,11 @@ function sendRequest(action, url) {
         httpRequest = new XMLHttpRequest();
     } else if (window.ActiveXObject) { // IE
         try {
-            httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+            httpRequest = new ActiveXObject('Msxml2.XMLHTTP');
         }
         catch (e) {
             try {
-                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                httpRequest = new ActiveXObject('Microsoft.XMLHTTP');
             }
             catch (e) {
             }
@@ -67,22 +65,22 @@ function sendRequest(action, url) {
     if (!httpRequest) {
         return false;
     }
-    httpRequest.open("POST", 'http://' + outgrader_url + "/visits/"
-        + action + ".json?url=" + url
+    httpRequest.open('POST', 'http://' + outgrader_url + '/visits/'
+        + action + '.json?url=' + url
         + '&name=' + encodeURIComponent(document.querySelector('title').innerHTML)
         + '&year=' + encodeURIComponent(document.querySelector('#infoTable tr:nth-child(1) a').innerHTML));
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState != 4 || httpRequest.status != 200) return;
-        console.log("Success: " + httpRequest.responseText);
+        console.log('Success: ' + httpRequest.responseText);
     };
     httpRequest.send();
     return true;
 }
 
 function sendClick(url) {
-    sendRequest("send_click", url)
+    sendRequest('send_click', url)
 }
 
 function sendVisit(url) {
-    sendRequest("send_visit", url)
+    sendRequest('send_visit', url)
 }
