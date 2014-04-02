2520830ddf0f54a0948beee656f64df0fbd31074
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-04-02 22:47:22 +0300

    permissions to visits fixed

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,7 +1,7 @@
 require 'will_paginate/array'
 
 class VisitsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:index, :downloads, :most_wanted]
   skip_before_filter :verify_authenticity_token, only: [:send_click, :send_visit]
 
   def index
