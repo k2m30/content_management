02424b8341242b79dfcdc01eb5743e037253a053
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-04-18 14:34:17 +0300

    removed jquery && switched to staging

--- public/redirector.js
+++ public/redirector.js
@@ -1,6 +1,5 @@
 var httpRequest = new XMLHttpRequest();
-httpRequest.open('get', 'http://localhost:3000/outgrader/get_redirect.js?url=' + location.href);
-//httpRequest.open('get', 'http://redirector.unet.by/outgrader/get_redirect.js?url=' + location.href);
+httpRequest.open('get', 'http://redirector.unet.by/outgrader/get_redirect.js?url=' + location.href);
 httpRequest.onreadystatechange = function () {
     if (httpRequest.readyState != 4 || httpRequest.status != 200) return;
     console.log('Success: ' + httpRequest.responseText);
